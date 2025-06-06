b1938f2491f7ee38d196b3a15bb8b910250be295
Yuvraj-1107-ML <yuvrajdawande373@gmail.com>
2025-06-06 14:08:15 +0530

    information added for deployment

--- password_encryption/settings.py
+++ password_encryption/settings.py
@@ -28,8 +28,15 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost','127.0.0.1','passwordencryptionapi-production.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS= ["https://passwordencryptionapi-production.up.railway.app/"]
+
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+    'localhost:8000'
+)
 
 # Application definition
 
